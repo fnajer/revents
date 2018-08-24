66f9704f113b6595680e1c0d9ef04dc7be7981c6
fnajer <lexactr@gmail.com>
2018-08-24 21:38:50 +0300

    Test two versions semantic-ui buttons in App

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Re-events</h1>
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          React button
+        </button>
+        <Button icon="smile" content="React button" />
       </div>
     );
   }
