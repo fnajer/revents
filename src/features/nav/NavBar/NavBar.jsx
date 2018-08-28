fb91ca9029ad4cd041b0e923427f73a73658c7c5
fnajer <lexactr@gmail.com>
2018-08-28 22:12:13 +0300

    Adding handle SignIn and SignOut components

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link } from 'react-router-dom';
 
+import SignedInMenu from '../Menus/SignedInMenu';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+
 class NavBar extends Component {
+  state = {
+    authenticated: true,
+  }
+
+  handleSignIn = () => {
+    this.setState({
+      authenticated: true,
+    });
+  }
+
+  handleSignOut = () => {
+    this.setState({
+      authenticated: false,
+    });
+  }
+
   render() {
+    const { authenticated } = this.state;
     return (
       <Menu inverted fixed="top">
         <Container>
@@ -16,10 +36,11 @@ class NavBar extends Component {
           <Menu.Item>
             <Button as={Link} to="/createForm" floated="right" positive inverted content="Create Event" />
           </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button basic inverted content="Sign Out" style={{marginLeft: '0.5em'}} />
-          </Menu.Item>
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
         </Container>
       </Menu>
     )
