357491597057913ce37579508707ff93183cf1f0
fnajer <lexactr@gmail.com>
2018-08-28 17:41:12 +0300

    Routing pages with React-router-dom

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,8 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom'
 
 import NavBar from '../../features/nav/NavBar/NavBar';
 import EventDashboard from '../../features/event/EventDashoard/EventDashboard';
+import HomePage from '../../features/home/HomePage';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
 
 class App extends Component {
   render() {
@@ -10,7 +17,13 @@ class App extends Component {
       <Fragment>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/events" component={EventDashboard} />
+          <Route path="/events/:id" component={EventDetailedPage} />
+          <Route path="/people" component={PeopleDashboard} />
+          <Route path="/profiles/:id" component={UserDetailedPage} />
+          <Route path="/settings" component={SettingsDashboard} />
+          <Route path="/createEvent" component={EventForm} />
         </Container>
       </Fragment>
     );
