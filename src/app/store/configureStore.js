485c0c48dee31ce75864c6a095f4078a65cd8c9a
fnajer <lexactr@gmail.com>
2018-08-29 21:47:32 +0300

    Setup rootReducer

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 
+import rootReducer from '../reducers/rootReducer';
+
 export const configureStore = (preloadedState) => {
   const middlewares = [];
   const middlewareEnhancer = applyMiddleware(...middlewares);
