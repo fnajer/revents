696933fbb96be65935f3d4132bb31d637fb63a35
fnajer <lexactr@gmail.com>
2018-09-04 16:32:37 +0300

    Fixed issue of scroll for transit to new page

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { configureStore } from './app/store/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
 
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import ScrollToTop from "./app/common/util/ScrollToTop";
+import App from "./app/layout/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 const store = configureStore();
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
-    rootEl);
+    rootEl
+  );
 };
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', () => {
+  module.hot.accept("./app/layout/App", () => {
     console.log(1222);
     setTimeout(render);
-  })
+  });
 }
 render();
 
