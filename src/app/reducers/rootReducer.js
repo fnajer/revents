327bb718e80bc0f188bb6fa3a2cdd74b8a64c58c
fnajer <lexactr@gmail.com>
2018-09-13 17:48:49 +0300

    Setup firebase: store and reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { firebaseReducer } from 'react-redux-firebase';
+import { firestoreReducer } from 'redux-firestore';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 import modalReducer from '../../features/modal/modalReducer';
@@ -7,6 +9,8 @@ import authReducer from '../../features/auth/authReducer';
 import asyncReducer from '../../features/async/asyncReducer';
 
 const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
   form: formReducer,
   test: testReducer,
   events: eventReducer,
