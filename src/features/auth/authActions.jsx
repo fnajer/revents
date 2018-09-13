bae333c7717934011b2d3b75bba98c0c632bbc8e
fnajer <lexactr@gmail.com>
2018-09-13 21:33:10 +0300

    Set signIn with firebase

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,15 +1,15 @@
-import { LOGIN_USER, SIGN_OUT_USER } from './authConstants';
+import { SIGN_OUT_USER } from './authConstants';
 import { closeModal } from '../modal/modalActions';
 
 export const loginUser = (creds) => {
-  return async dispatch => {
-    dispatch({
-      type: LOGIN_USER,
-      payload: {
-        creds,
-      },
-    });
-    dispatch(closeModal());
+  return async (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+      dispatch(closeModal());
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
