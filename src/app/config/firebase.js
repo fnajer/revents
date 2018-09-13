fbba495fdab21bcd934880362f9f36cbacdc01b7
fnajer <lexactr@gmail.com>
2018-09-13 16:10:51 +0300

    Connect config firebase to app

---
+++ src/app/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCi4yTikFXCW_HbYvau9zZIYU7kAihoK3Q",
+  authDomain: "revents-5b101.firebaseapp.com",
+  databaseURL: "https://revents-5b101.firebaseio.com",
+  projectId: "revents-5b101",
+  storageBucket: "revents-5b101.appspot.com",
+  messagingSenderId: "230774625113"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
