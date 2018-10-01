fa5f7a2ffd9218ee6c6c3b4940111630fe5413db
fnajer <lexactr@gmail.com>
2018-10-01 22:56:53 +0300

    UserDetailedDescription with dynamic data

--- src/features/user/UserDetailed/UserDetailedDescription.jsx
+++ src/features/user/UserDetailed/UserDetailedDescription.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Grid, Segment, Header, List, Item, Icon } from 'semantic-ui-react';
+import format from 'date-fns/format';
 
-const UserDetailedDescription = () => {
+const UserDetailedDescription = ({profile}) => {
+  let createdAt;
+  if (profile.createdAt) {
+    createdAt = format(profile.createdAt.toDate(), 'D MMM, YYYY');
+  }
   return (
     <Grid.Column width={12}>
       <Segment>
@@ -9,32 +14,32 @@ const UserDetailedDescription = () => {
           <Grid.Column width={10}>
             <Header icon="smile" content="About Display Name" />
             <p>
-              I am a: <strong>Occupation Placeholder</strong>
+              I am a: <strong>{profile.about || 'tbn'}</strong>
             </p>
             <p>
-              Originally from <strong>United Kingdom</strong>
+              Originally from <strong>{profile.origin || 'tbn'}</strong>
             </p>
             <p>
-              Member Since: <strong>28th March 2018</strong>
+              Member Since: <strong>{createdAt}</strong>
             </p>
-            <p>Description of user</p>
+            <p>{profile.description}</p>
           </Grid.Column>
           <Grid.Column width={6}>
             <Header icon="heart outline" content="Interests" />
-            <List>
-              <Item>
-                <Icon name="heart" />
-                <Item.Content>Interest 1</Item.Content>
-              </Item>
-              <Item>
-                <Icon name="heart" />
-                <Item.Content>Interest 2</Item.Content>
-              </Item>
-              <Item>
-                <Icon name="heart" />
-                <Item.Content>Interest 3</Item.Content>
-              </Item>
-            </List>
+            {
+              profile.interests ?
+              <List>
+              {
+                profile.interests &&
+                profile.interests.map((interest, index) => (
+                  <Item key={index}>
+                    <Icon name="heart" />
+                    <Item.Content>{interest}</Item.Content>
+                  </Item>
+                ))
+              }
+              </List> : <p>No interests</p>
+            }
           </Grid.Column>
         </Grid>
       </Segment>
