7db368c2f98cc3f4451295ebadbbe1ef142b4d30
fnajer <lexactr@gmail.com>
2018-10-05 15:18:52 +0300

    Improve interactive with cancel button and fix venueLatLng

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -68,12 +68,12 @@ class EventForm extends Component {
 
   async componentDidMount() {
     const { firestore, match } = this.props;
-    let event = await firestore.get(`events/${match.params.id}`);
-    if (event.exists) {
-      this.setState({
-        venueLatLng: event.data().venueLatLng,
-      });
-    }
+    await firestore.setListener(`events/${match.params.id}`);
+    // if (event.exists) {
+    //   this.setState({
+    //     venueLatLng: event.data().venueLatLng,
+    //   });
+    // }
   }
 
   handleLoadScript = () => {
@@ -112,6 +112,9 @@ class EventForm extends Component {
     values.venueLatLng = this.state.venueLatLng;
 
     if (this.props.initialValues.id) {
+      if (Object.keys(values.venueLatLng).length === 0) {
+        values.venueLatLng = this.props.event.venueLatLng;
+      }
       this.props.updateEvent(values);
       this.props.history.goBack();
     } else {
