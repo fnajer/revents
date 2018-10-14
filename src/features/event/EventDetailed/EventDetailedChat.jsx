a8e041b6c066164a3e8ef341e477cf7bd14ea6a8
fnajer <lexactr@gmail.com>
2018-10-14 11:03:22 +0300

    Binding form for reply

--- src/features/event/EventDetailed/EventDetailedChat.jsx
+++ src/features/event/EventDetailed/EventDetailedChat.jsx
@@ -1,49 +1,73 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Segment, Comment, Header } from 'semantic-ui-react';
+import { Segment, Comment, Header } from "semantic-ui-react";
 import distanceInWords from "date-fns/distance_in_words";
 
-import EventDetailedChatForm from './EventDetailedChatForm';
+import EventDetailedChatForm from "./EventDetailedChatForm";
 
-const EventDetailedChat = ({ eventId, addEventComment, eventChat }) => {
-  return (
-    <div>
-      <Segment
-        textAlign="center"
-        attached="top"
-        inverted
-        color="teal"
-        style={{ border: 'none' }}
-      >
-        <Header>Chat about this event</Header>
-      </Segment>
+export class EventDetailedChat extends Component {
+  state = {
+    showReplyForm: false,
+  }
 
-      <Segment attached>
-        <Comment.Group>
-          {
-            eventChat && eventChat.map(comment => (
-              <Comment key={comment.id}>
-                <Comment.Avatar src={comment.photoURL} />
-                <Comment.Content>
-                  <Comment.Author as={Link} to={`/profile/${comment.uid}`}>{comment.displayName}</Comment.Author>
-                  <Comment.Metadata>
-                    <div>{distanceInWords(comment.date, Date.now())}</div>
-                  </Comment.Metadata>
-                  <Comment.Text>{comment.text}</Comment.Text>
-                  <Comment.Actions>
-                    <Comment.Action>Reply</Comment.Action>
-                  </Comment.Actions>
-                </Comment.Content>
-              </Comment>
-            ))
-          }
-        </Comment.Group>
-        
-        <EventDetailedChatForm eventId={eventId} addEventComment={addEventComment}/>
-      </Segment>
-    </div>
-  )
+  handleOpenReplyForm = () => {
+    this.setState({
+      showReplyForm: true,
+    });
+  }
+
+  render() {
+    const { eventId, addEventComment, eventChat } = this.props;
+    const { showReplyForm } = this.state;
+    return (
+      <div>
+        <Segment
+          textAlign="center"
+          attached="top"
+          inverted
+          color="teal"
+          style={{ border: "none" }}
+        >
+          <Header>Chat about this event</Header>
+        </Segment>
+
+        <Segment attached>
+          <Comment.Group>
+            {eventChat &&
+              eventChat.map(comment => (
+                <Comment key={comment.id}>
+                  <Comment.Avatar src={comment.photoURL} />
+                  <Comment.Content>
+                    <Comment.Author as={Link} to={`/profile/${comment.uid}`}>
+                      {comment.displayName}
+                    </Comment.Author>
+                    <Comment.Metadata>
+                      <div>{distanceInWords(comment.date, Date.now())}</div>
+                    </Comment.Metadata>
+                    <Comment.Text>{comment.text}</Comment.Text>
+                    <Comment.Actions>
+                      <Comment.Action onClick={this.handleOpenReplyForm}>Reply</Comment.Action>
+                      {
+                        showReplyForm &&
+                        <EventDetailedChatForm 
+                          eventId={eventId}
+                          addEventComment={addEventComment} 
+                        />
+                      }
+                    </Comment.Actions>
+                  </Comment.Content>
+                </Comment>
+              ))}
+          </Comment.Group>
+
+          <EventDetailedChatForm
+            eventId={eventId}
+            addEventComment={addEventComment}
+          />
+        </Segment>
+      </div>
+    );
+  }
 }
 
 export default EventDetailedChat;
-
