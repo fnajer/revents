be806c01267c79355210b80fa46eaa17cd80b88f
fnajer <lexactr@gmail.com>
2018-10-14 12:21:24 +0300

    View one level deep replies for comments

--- src/features/event/EventDetailed/EventDetailedChat.jsx
+++ src/features/event/EventDetailed/EventDetailedChat.jsx
@@ -68,6 +68,37 @@ export class EventDetailedChat extends Component {
                       }
                     </Comment.Actions>
                   </Comment.Content>
+                  {
+                    comment.childNodes && comment.childNodes.map(child => (
+                    <Comment.Group>
+                      <Comment key={child.id}>
+                        <Comment.Avatar src={child.photoURL} />
+                        <Comment.Content>
+                          <Comment.Author as={Link} to={`/profile/${child.uid}`}>
+                            {child.displayName}
+                          </Comment.Author>
+                          <Comment.Metadata>
+                            <div>{distanceInWords(child.date, Date.now())}</div>
+                          </Comment.Metadata>
+                          <Comment.Text>{child.text}</Comment.Text>
+                          <Comment.Actions>
+                            <Comment.Action onClick={this.handleOpenReplyForm(child.id)}>Reply</Comment.Action>
+                            {
+                              showReplyForm && selectedCommentId === child.id &&
+                              <EventDetailedChatForm 
+                                eventId={eventId}
+                                addEventComment={addEventComment}
+                                form={`reply_${child.id}`}
+                                closeForm={this.handleCloseReplyForm}
+                                parentId={child.parentId}
+                              />
+                            }
+                          </Comment.Actions>
+                        </Comment.Content>
+                      </Comment>
+                    </Comment.Group>
+                    ))
+                  }
                 </Comment>
               ))}
           </Comment.Group>
