b00a2bd9738f68741c86db47ec6ec6c4a59a6822
fnajer <lexactr@gmail.com>
2018-10-15 19:30:21 +0300

    Create cancelActivity cloud function

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+const newActivity = (type, event, id) => {
+  return {
+    type: type,
+    title: event.title,
+    hostedBy: event.hostedBy,
+    eventDate: event.date,
+    photoURL: event.hostPhotoURL,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    hostUid: event.hostUid,
+    eventId: id,
+  };
+}
+
 exports.createActivity = functions.firestore
   .document('events/{eventId}')
   .onCreate(event => {
@@ -9,16 +22,7 @@ exports.createActivity = functions.firestore
 
     console.log(newEvent);
     
-    const activity = {
-      type: 'newEvent',
-      title: newEvent.title,
-      hostedBy: newEvent.hostedBy,
-      eventDate: newEvent.date,
-      photoURL: newEvent.photoURL,
-      timestamp: admin.firestore.FieldValue.serverTimestamp(),
-      hostUid: newEvent.hostUid,
-      eventId: event.id,
-    };
+    const activity = newActivity('newEvent', newEvent, event.id);
 
     console.log(activity);
 
@@ -29,5 +33,30 @@ exports.createActivity = functions.firestore
       })
       .catch(error => {
         return console.log('Error adding activity', error);
-      })
+      });
   });
+
+  exports.cancelActivity = functions.firestore
+    .document('events/{eventId}')
+    .onUpdate((event, context) => {
+      const updatedEvent = event.after.data();
+      const previousEventData = event.before.data();
+      console.log({event});
+      console.log({context});
+      console.log({updatedEvent});
+      console.log({previousEventData});
+
+      if (!updatedEvent.cancelled || updatedEvent.cancelled === previousEventData.cancelled) return false;
+
+      const activity = newActivity('cancelledEvent', updatedEvent, context.params.eventId);
+
+      return admin.firestore().collection('activity')
+        .add(activity)
+        .then(docRef => {
+          return console.log('Activity created with ID:', docRef.id);
+        })
+        .catch(error => {
+          return console.log('Error adding activity', error);
+        });
+
+    });
